f35e9f81224fecefd3e7aa1a5d131242ef4ff400
conary <conary@hotmail.es>
2019-06-17 23:22:31 -0600

    Firebase CLi

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,7 +4,10 @@ import { reduxForm, Field } from 'redux-form';
  let SearchBar = () => {
   return (
     <form>
-        <Field name="searchKeyWord" component="input"/>
+        <Field name="searchKeyWord" component="input" placeholder="Search"/>
+        <button className="search-btn">
+          <i>S</i>
+        </button>
     </form>
   )
 }
